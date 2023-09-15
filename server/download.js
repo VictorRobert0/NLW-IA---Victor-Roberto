97d947b031403425b8a395bc75f898ce3c03a88f
Victor Roberto <contatokataltd@gmail.com>
2023-09-15 13:58:10 -0300

    Shorts Summary COMPLETO COM IA

--- server/download.js
+++ server/download.js
@@ -1,7 +1,7 @@
 import ytdl from "ytdl-core";           
 import fs from 'fs'
 
-export const download = (videoId) => {
+export const download = (videoId) => new Promise ((resolve,reject) => {
   const videoURL = "https://www.youtube.com/shorts/" + videoId
   console.log ("Realizando o download do video:", videoId)
 
@@ -14,8 +14,10 @@ export const download = (videoId) => {
   })
   .on("end", () => {
     console.log( "Download finalizado com sucesso.")
+    resolve()
   })
   .on("error,", (error) => {
     console.log("Não foi possível fazer o download do vídeo. Detalhes do erro:", error)
+    reject(error)
   }).pipe(fs.createWriteStream("./tmp/audio.mp4"))
-}
+})
